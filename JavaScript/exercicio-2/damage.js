83ee74cd7e898fa0965e60b02ba9985b8f37c480
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-02 16:11:06 -0300

    Update damage.js

--- JavaScript/exercicio-2/damage.js
+++ JavaScript/exercicio-2/damage.js
@@ -5,3 +5,22 @@ const defender = prompt("Who is the defender Pokemon?")
 let lifePoints = parseFloat(prompt("How many life points " + defender + " has?"))
 const defensePower = parseFloat(prompt("How many Defensive Points " + defender + " has?"))
 const shield = prompt("Does " + defender + "has Shield? (Yes/No)")
+
+let damageOutput = 0
+
+if (attackPower > defensePower && shield === "no") {
+    damageOutput = attackPower - defensePower
+} else if (attackPower > defensePower && shield === "yes") {
+    damageOutput = (attackPower - defensePower) / 2
+}
+
+lifePoints -= damageOutput
+
+alert(striker + " caused " + damageOutput + " damage on " + defender)
+alert(
+    striker + "\nAttack Power: " + attackPower +"\n\n" +
+    defender + "\nLife Points: " + lifePoints +
+    "\nDefense Points: " + defensePower + "\nShields: " + shield
+)
+
+
