bff613b5e1021cce4d447b74873fc997c50808f0
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-02 15:57:17 -0300

    Update damage.js

---
+++ JavaScript/exercicio-2/damage.js
@@ -0,0 +1,7 @@
+const striker = prompt("Who is the Striker Pokemon")
+const attackPower = parseFloat(prompt("What's the Attack Power of " + striker))
+
+const defender = prompt("Who is the defender Pokemon?")
+let lifePoints = parseFloat(prompt("How many life points " + defender + " has?"))
+const defensePower = parseFloat(prompt("How many Defensive Points " + defender + " has?"))
+const shield = prompt("Does " + defender + "has Shield? (Yes/No)")
