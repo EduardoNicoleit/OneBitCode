0f1ebdd9c682d3ca4eb7972b9448da4c98103474
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-02 15:49:06 -0300

    Task 2

--- JavaScript/exercicio-2/velocity.js
+++ JavaScript/exercicio-2/velocity.js
@@ -3,3 +3,11 @@ const velocity1 = parseFloat(prompt("What's the velocity of the First Car?"))
 
 const car2 = prompt("What's the name of the Second Car?")
 const velocity2 = parseFloat(prompt("What's the velocity of the Second Car?"))
+
+ if (velocity1 > velocity2) {
+    alert(car1 + " is faster than " + car2)    
+ } else if (velocity2 > velocity1) {
+    alert(car2 + " is faster than " + car1)
+ } else {
+    alert(car1 + " and " + car2 + " have the same speed!")
+ }
