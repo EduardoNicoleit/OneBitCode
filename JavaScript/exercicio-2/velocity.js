1ea10b862ec1b7e68b84a1bd7b4195312f4a9b6f
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-02 15:42:37 -0300

    Task One

---
+++ JavaScript/exercicio-2/velocity.js
@@ -0,0 +1,5 @@
+const car1 = prompt("What's the name of the First Car?")
+const velocity1 = parseFloat(prompt("What's the velocity of the First Car?"))
+
+const car2 = prompt("What's the name of the Second Car?")
+const velocity2 = parseFloat(prompt("What's the velocity of the Second Car?"))
