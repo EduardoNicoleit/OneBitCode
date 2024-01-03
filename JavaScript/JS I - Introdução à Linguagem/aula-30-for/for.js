ef071884216dc0d0677d0e30dc0787036ffe3c18
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-03 16:41:11 -0300

    addying a countdown

---
+++ JavaScript/JS I - Introdução à Linguagem/aula-30-for/for.js
@@ -0,0 +1,19 @@
+let indice = 0
+let liftOff = 10
+
+for (alert("Inicializando contagem!"); indice <= 10; indice++) 
+{
+    alert("Indice = " + indice)   
+}
+
+
+for (alert("Iniciando contagem Regressiva!!"); liftOff >= 0; liftOff--){
+    alert("Indice = " + liftOff)
+}
+
+alert("Liftoff!!")
+
+
+
+
+
