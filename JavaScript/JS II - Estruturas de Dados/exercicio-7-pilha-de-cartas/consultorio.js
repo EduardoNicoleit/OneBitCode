ae17d92d89d53ac894f505c95b644edfdd28e30b
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-06 10:42:28 -0300

    Update consultorio.js

--- JavaScript/JS II - Estruturas de Dados/exercicio-7-pilha-de-cartas/consultorio.js
+++ JavaScript/JS II - Estruturas de Dados/exercicio-7-pilha-de-cartas/consultorio.js
@@ -1,1 +1,15 @@
 let fila = []
+let opcao = ""
+
+do {
+    let pacientes = ""
+    for (let i = 0; i< fila.length; i++) {
+        pacientes += (i + 1) + "º - " + fila[i] + "\n"
+    }
+
+    opcao = prompt(
+        "Pacientes: \n" + pacientes +
+        "\nEscolha uma ação: \n'. Novo Paciente\n2. Consultar paciente\n3. Sair"
+    )
+
+} while(opcao !== "3")
