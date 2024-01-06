6c69120fd69380af5da287719ea52ddfd1d1a5ba
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-06 10:46:49 -0300

    Update consultorio.js

--- JavaScript/JS II - Estruturas de Dados/exercicio-7-pilha-de-cartas/consultorio.js
+++ JavaScript/JS II - Estruturas de Dados/exercicio-7-pilha-de-cartas/consultorio.js
@@ -12,4 +12,20 @@ do {
         "\nEscolha uma ação: \n'. Novo Paciente\n2. Consultar paciente\n3. Sair"
     )
 
+    switch (opcao) {
+        case "1":
+            const novoPaciente = prompt("Qual o nome do Paciente?")
+            fila.push(novoPaciente)
+            break
+        case "2":
+            const pacienteConsultado = fila.shift()
+            alert(pacienteConsultado + "foi removido da fila")
+            break
+        case "3":
+            alert("Encerrando...")
+            break
+        default:
+            alert("Opção invalida!")    
+    }
+
 } while(opcao !== "3")
