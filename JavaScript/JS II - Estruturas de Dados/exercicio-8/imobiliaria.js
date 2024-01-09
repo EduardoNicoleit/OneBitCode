e5fca47c6dc5288083fe34c62244064d8005ab9e
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-01-09 09:44:43 -0300

    Update imobiliaria.js

--- JavaScript/JS II - Estruturas de Dados/exercicio-8/imobiliaria.js
+++ JavaScript/JS II - Estruturas de Dados/exercicio-8/imobiliaria.js
@@ -3,6 +3,8 @@ let opcao = ""
 
 do {
     opcao = prompt(
-        "Bem Vindo ao cadastro de imóveis"
+        "Bem Vindo(a) ao cadastro de imóveis!\n" +
+        "Total de Imóvies: " + imoveis.length +
+        "\n\nEscolha uma Opção:\n1. Novo Imóvel\n2. Lista de Imóvies\n.3 Sair"
     )
 } while (opcao !== "3")
