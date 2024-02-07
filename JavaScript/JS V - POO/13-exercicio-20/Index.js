b8e779b2d554b3310d04529ae3f35d98fdc608f3
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-07 14:19:08 -0300

    Update Index.js

--- JavaScript/JS V - POO/13-exercicio-20/Index.js
+++ JavaScript/JS V - POO/13-exercicio-20/Index.js
@@ -1,4 +1,4 @@
-import { Component } from "./Component.js"
+import { Component } from "/Component.js"
 import { Form } from "./Form.js"
 import { Input } from "./Input.js"
 import { Label } from "./Label.js"
