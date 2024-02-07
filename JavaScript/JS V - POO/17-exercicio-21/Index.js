db41909afe5109c9183ec95c3a3b429f13730395
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-07 15:24:06 -0300

    Update Index.js

--- JavaScript/JS V - POO/17-exercicio-21/Index.js
+++ JavaScript/JS V - POO/17-exercicio-21/Index.js
@@ -7,3 +7,10 @@ const beatrice = new Thief('Beatrice', 140, 22, 8)
 const cain = new Warrior('Cain', 200, 14, 12, 4)
 
 console.table({arthur, beatrice, cain})
+
+cain.switchStance()
+arthur.attack(cain)
+beatrice.attack(arthur)
+
+console.table({arthur, beatrice, cain})
+
