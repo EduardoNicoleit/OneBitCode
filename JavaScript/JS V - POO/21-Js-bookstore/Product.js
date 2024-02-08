9ea184a8d283bce19789915cb00f191a6c8fe25a
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-08 14:11:07 -0300

    Update Product.js

---
+++ JavaScript/JS V - POO/21-Js-bookstore/Product.js
@@ -0,0 +1,8 @@
+module.exports = class Product {
+    constructor(name, description, price, inStock = 0) {
+        this.name = name
+        this.description = description
+        this.price = price
+        this.inStock = inStock
+    }
+}
