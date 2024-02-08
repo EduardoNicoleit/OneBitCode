3137612ded77f2193293c727766110b593314694
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-08 15:39:16 -0300

    Update App.js

--- JavaScript/JS V - POO/21-Js-bookstore/App.js
+++ JavaScript/JS V - POO/21-Js-bookstore/App.js
@@ -1,5 +1,24 @@
+const User = require("../entities/User")
 const Database = require("./Database")
 
 module.exports = class App {
     static #database = new Database()
+
+    createUser(name, email, password) {
+        const user = new User(name, email, password)
+        App.#database.saveUser(user)
+    }
+    
+    getUsers() {
+        App.#database.find('users')
+    }
+
+    createAuthor(name, nationality, bio) {
+        const author = new Author(name, nationality, bio)
+        App.#database.saveAuthor(author)
+    }
+
+    getAuthor() {
+        
+    }
 }
