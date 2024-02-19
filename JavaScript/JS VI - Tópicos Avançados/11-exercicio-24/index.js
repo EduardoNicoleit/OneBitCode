9b661321a55639f69bd03dd3c80e4569a4bb5b47
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-19 09:43:48 -0300

    Update index.js

--- JavaScript/JS VI - Tópicos Avançados/11-exercicio-24/index.js
+++ JavaScript/JS VI - Tópicos Avançados/11-exercicio-24/index.js
@@ -7,4 +7,18 @@ function imc(weight, height) {
     })
   }
 
+  function showImc(weight, height) {
+    imc(weight, height).then((result) => {
+      console.log(`O resultado do IMC foi de ${result}.`)
+  
+      if (result < 18.5) console.log('Situação: MAGREZA')
+      else if (result < 25) console.log('Situação: NORMAL')
+      else if (result < 30) console.log('Situação: SOBREPESO')
+      else if (result < 40) console.log('Situação: OBESIDADE')
+      else console.log('Situação: OBESIDADE GRAVE')
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
   
