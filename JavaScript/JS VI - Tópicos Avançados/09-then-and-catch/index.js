fcbf520a8bf000a99f5068e7150ebd5bd05716f8
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-19 09:27:48 -0300

    Update index.js

--- JavaScript/JS VI - Tópicos Avançados/09-then-and-catch/index.js
+++ JavaScript/JS VI - Tópicos Avançados/09-then-and-catch/index.js
@@ -2,8 +2,12 @@ function execute() {
     return new Promise((resolve, reject) => {
         console.log('A promise está sendo executada.')
         setTimeout(() => {
-            console.log('Resolvendo a promise...')
-            resolve('Resultado')
+            if (1 + 1 === 2) {
+                reject('1 + 1 não é igual a 2')
+            } else {
+                console.log('Resolvendo a promise...')
+                resolve('Resultado')
+            }
         }, 3 * 1000)
     })
 }
@@ -12,4 +16,6 @@ const p = execute()
 
 p.then((result) => {
     console.log(`A promise foi resolvida. O resultado foi: ${result}`)
+}).catch((err) => {
+    console.log(`A promise foi rejeitada! Motivo: ${err}`)
 })
