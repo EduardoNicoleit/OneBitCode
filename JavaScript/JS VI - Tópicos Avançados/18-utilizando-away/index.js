f9cd2e75b1e2b8dd04bd6868ebbc713b43f74d50
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-20 12:52:25 -0300

    Update index.js

--- JavaScript/JS VI - Tópicos Avançados/18-utilizando-away/index.js
+++ JavaScript/JS VI - Tópicos Avançados/18-utilizando-away/index.js
@@ -11,3 +11,25 @@ async function execute() {
 }
 
 execute()
+
+// ...
+
+async function asyncSubtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return Promise.reject('arguments must be of type number')
+    }
+    return a - b
+}
+
+async function execute() {
+    try {
+        const sumResult = await asyncSum(50, 33)
+        console.log(sumResult)
+        const subtractResult = await asyncSubtract(50, null)
+        console.log(subtractResult)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+execute()
