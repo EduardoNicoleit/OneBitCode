3cafe5aae86b2accc2113659e0da73dd4f984be8
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2024-02-20 13:16:30 -0300

    Create index.js

---
+++ JavaScript/JS VI - Tópicos Avançados/20-exercicio 24/index.js
@@ -0,0 +1,6 @@
+async function imc(weight, height) {
+    if (typeof weight  !== 'number' || typeof height !== 'number') {
+      return Promise.reject('arguments must be of type number')
+    }
+    return weight / (height * height)
+  }
